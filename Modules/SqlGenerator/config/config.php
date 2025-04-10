0094711bafada6184fd34fcdd6a4d4ef52aa89d3
yoongsinjie <sinjie2008@gmail.com>
2025-04-10 22:07:41 +0800

    Boiler Plate Default Function
    Admin Protal
    - Theme AdminLTE
    -  Role Premission
    -  Activity Log
    -  Pagination
    -  Search
    -  Query Builder
    -  Backup
    -  Media
    -  AI Chat to db
    -  API Passport
    
    Modules Convert
    - Todo List
    - AI SQL Generator
    
    Sample Config
    - .env

--- Modules/SqlGenerator/config/config.php
+++ Modules/SqlGenerator/config/config.php
@@ -2,6 +2,6 @@
 
 return [
     'name' => 'SqlGenerator', // Added module name
-    'api_url' => env('SQL_GENERATOR_API_URL', 'https://free.v36.cm/v1/chat/completions'),
-    'api_key' => env('SQL_GENERATOR_API_KEY', 'sk-tKpd9XCw9jdX2Bm1B577Dd79F2704bEdA21e6e0d089c25F9'),
+    'api_url' => env('SQL_GENERATOR_API_URL', ''),
+    'api_key' => env('SQL_GENERATOR_API_KEY', ''),
 ];
